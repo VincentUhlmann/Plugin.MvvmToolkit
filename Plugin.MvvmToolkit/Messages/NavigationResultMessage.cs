b79e61ba1b83213e7609a6e8f23483544df42d40
Vincent Uhlmann <contact@vuhlmann.com>
2023-08-09 14:35:45 +0200

    Fixed code generation errors

--- Plugin.MvvmToolkit/Messages/NavigationResultMessage.cs
+++ Plugin.MvvmToolkit/Messages/NavigationResultMessage.cs
@@ -1,11 +1,9 @@
-﻿[assembly: InternalsVisibleTo("Plugin.MvvmToolkit.Maui")]
-
-namespace Plugin.MvvmToolkit.Messages;
+﻿namespace Plugin.MvvmToolkit.Messages;
 
 /// <summary>
 /// A message that is sent when a navigation occurs.
 /// </summary>
 /// <typeparam name="TResult"></typeparam>
-internal sealed class NavigationResultMessage<TResult> : AsyncRequestMessage<TResult>
+public sealed class NavigationResultMessage<TResult> : AsyncRequestMessage<TResult>
 {
 }
