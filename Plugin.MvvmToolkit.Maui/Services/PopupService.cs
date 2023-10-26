287fcc3bdde139946203a9ca4af5d17db446e744
Vincent Uhlmann <contact@vuhlmann.com>
2023-10-26 22:02:41 +0200

    Simplified the IPopupService

--- Plugin.MvvmToolkit.Maui/Services/PopupService.cs
+++ Plugin.MvvmToolkit.Maui/Services/PopupService.cs
@@ -4,20 +4,20 @@
 public sealed class PopupService : IPopupService
 {
     /// <inheritdoc />
-    public async Task DisplayMessagePopupAsync(MessagePopup popup)
+    public async Task DisplayMessagePopupAsync(string title, string message, string cancel)
     {
         if (Application.Current is null || Application.Current.MainPage is null)
             throw new InvalidOperationException("Application.Current.MainPage is null.");
 
-        await Application.Current.MainPage.DisplayAlert(popup.Title, popup.Message, popup.Cancel);
+        await Application.Current.MainPage.DisplayAlert(title, message, cancel);
     }
 
     /// <inheritdoc />
-    public async Task<bool> DisplayConfirmationPopupAsync(ConfirmationPopup popup)
+    public async Task<bool> DisplayConfirmationPopupAsync(string title, string message, string accept, string cancel)
     {
         if (Application.Current is null || Application.Current.MainPage is null)
             throw new InvalidOperationException("Application.Current.MainPage is null.");
 
-        return await Application.Current.MainPage.DisplayAlert(popup.Title, popup.Message, popup.Accept, popup.Cancel);
+        return await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
     }
 }
--- Plugin.MvvmToolkit/Abstractions/IPopupService.cs
+++ Plugin.MvvmToolkit/Abstractions/IPopupService.cs
@@ -8,13 +8,18 @@ public interface IPopupService
     /// <summary>
     /// Displays a message popup.
     /// </summary>
-    /// <param name="popup">The message popup to display.</param>
-    Task DisplayMessagePopupAsync(MessagePopup popup);
+    /// <param name="title">The title of the popup.</param>
+    /// <param name="message">The message of the popup.</param>
+    /// <param name="cancel">The text of the cancel button.</param>
+    Task DisplayMessagePopupAsync(string title, string message, string cancel);
 
     /// <summary>
     /// Displays a confirmation popup.
     /// </summary>
-    /// <param name="popup">The confirmation popup to display.</param>
-    /// <returns>True if the user accepted the confirmation, false otherwise.</returns>
-    Task<bool> DisplayConfirmationPopupAsync(ConfirmationPopup popup);
+    /// <param name="title">The title of the popup.</param>
+    /// <param name="message">The message of the popup.</param>
+    /// <param name="accept">The text of the accept button.</param>
+    /// <param name="cancel">The text of the cancel button.</param>
+    /// <returns>If the user accepted the popup.</returns>
+    Task<bool> DisplayConfirmationPopupAsync(string title, string message, string accept, string cancel);
 }
