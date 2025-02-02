e2a1c2968e2ca88a81227b10bd9c727114e8e001
Vincent Uhlmann <contact@vuhlmann.com>
2025-02-02 22:28:22 +0100

    Updated to .net9

--- Plugin.MvvmToolkit.Maui/Services/PopupService.cs
+++ Plugin.MvvmToolkit.Maui/Services/PopupService.cs
@@ -6,18 +6,18 @@ public sealed class PopupService : IPopupService
     /// <inheritdoc />
     public async Task DisplayMessagePopupAsync(string title, string message, string cancel)
     {
-        if (Application.Current is null || Application.Current.MainPage is null)
-            throw new InvalidOperationException("Application.Current.MainPage is null.");
+        if (Application.Current is null || Application.Current.Windows[0]?.Page is null)
+            throw new InvalidOperationException("Application.Current.WIndows[0].Page is null.");
 
-        await Application.Current.MainPage.DisplayAlert(title, message, cancel);
+        await Application.Current!.Windows[0]!.Page!.DisplayAlert(title, message, cancel);
     }
 
     /// <inheritdoc />
     public async Task<bool> DisplayConfirmationPopupAsync(string title, string message, string accept, string cancel)
     {
-        if (Application.Current is null || Application.Current.MainPage is null)
-            throw new InvalidOperationException("Application.Current.MainPage is null.");
+        if (Application.Current is null || Application.Current.Windows[0]?.Page is null)
+            throw new InvalidOperationException("Application.Current.WIndows[0].Page is null.");
 
-        return await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+        return await Application.Current!.Windows[0]!.Page!.DisplayAlert(title, message, accept, cancel);
     }
 }
